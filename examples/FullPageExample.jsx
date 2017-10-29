d6da10215250a71a14547b9849848947e648f495
zwug <zwugme@gmail.com>
2017-10-29 23:04:39 +0300

    code refactoring

--- examples/FullPageExample.jsx
+++ examples/FullPageExample.jsx
@@ -1,61 +1,45 @@
 import React from 'react';
 import { FullPage, Slide } from '../src';
 
-export default class FullPageExample extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.fullpageInstance = null;
-  }
-
-  onPrev = () => {
-    this.fullpageInstance.scrollPrev();
-  }
-
-  onNext = () => {
-    this.fullpageInstance.scrollNext();
-  }
-
-  render() {
-    const controlsProps = {
-      style: {
-        left: '50%',
-        paddingTop: '10px',
-        position: 'fixed',
-        transform: 'translateX(-50%)',
-      },
-    };
-    return (
-      <FullPage controls controlsProps={controlsProps}>
-        <Slide
-          style={{
+export default function FullPageExample() {
+  const controlsProps = {
+    style: {
+      left: '50%',
+      paddingTop: '10px',
+      position: 'fixed',
+      transform: 'translateX(-50%)',
+    },
+  };
+  return (
+    <FullPage controls controlsProps={controlsProps}>
+      <Slide
+        style={{
             background: '#2ECC40', display: 'flex', alignItems: 'center', justifyContent: 'center',
           }}
-        >
-          <h1>1</h1>
-        </Slide>
-        <Slide
-          style={{
+      >
+        <h1>1</h1>
+      </Slide>
+      <Slide
+        style={{
             background: '#0074D9', display: 'flex', alignItems: 'center', justifyContent: 'center',
           }}
-        >
-          <h1>2</h1>
-        </Slide>
-        <Slide
-          style={{
+      >
+        <h1>2</h1>
+      </Slide>
+      <Slide
+        style={{
            background: '#00c4ff', display: 'flex', alignItems: 'center', justifyContent: 'center',
           }}
-        >
-          <h1>3</h1>
-        </Slide>
-        <Slide
-          style={{
+      >
+        <h1>3</h1>
+      </Slide>
+      <Slide
+        style={{
             background: '#d52685', display: 'flex', alignItems: 'center', justifyContent: 'center',
           }}
-        >
-          <h1>4</h1>
-        </Slide>
-      </FullPage>
-    );
-  }
+      >
+        <h1>4</h1>
+      </Slide>
+    </FullPage>
+  );
 }
