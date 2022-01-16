fb064825efb8f478de2c2e03d8e3724e405a52c1
Alpha-1004 <66864997+Alpha-1004@users.noreply.github.com>
2022-01-16 13:23:08 +0900

    feat: smoother scroll fixes issue #36

--- src/utils/animated-scroll-to.js
+++ src/utils/animated-scroll-to.js
@@ -1,21 +1,14 @@
-import easeInOutCubic from './ease-in-out-cubic';
-
-export default function animatedScrollTo(scrollTo, duration, callback) {
-  const scrollFrom = window.scrollY || window.pageYOffset || 0;
-  const scrollDiff = scrollTo - scrollFrom;
-  let currentTime = 0;
-  const increment = 20;
-
-  (function animateScroll() {
-    currentTime += increment;
-    const newScrollPos = easeInOutCubic(currentTime, scrollFrom, scrollDiff, duration);
-
-    window.scrollTo(0, newScrollPos);
-    if (currentTime > duration) {
+export default function animatedScrollTo(scrollTo, callback) {
+  const scrollCallback = (e) => {
+    if (window.scrollY.toFixed() === scrollTo.toFixed()) {
+      window.removeEventListener("scroll", scrollCallback);
       callback();
-      return;
     }
-
-    setTimeout(animateScroll, increment);
-  }());
+  };
+  window.addEventListener("scroll", scrollCallback);
+  scrollCallback();
+  window.scrollTo({
+    top: scrollTo,
+    behavior: "smooth",
+  });
 }
