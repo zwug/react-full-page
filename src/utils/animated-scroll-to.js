5c9d12a90c9e4e0ddc1e372ef98c2538a4d2ddff
Alpha-1004 <66864997+Alpha-1004@users.noreply.github.com>
2022-01-16 13:40:02 +0900

    fix: made callback fire instantly (would fix mouse interuptions)

--- src/utils/animated-scroll-to.js
+++ src/utils/animated-scroll-to.js
@@ -1,14 +1,7 @@
 export default function animatedScrollTo(scrollTo, callback) {
-  const scrollCallback = (e) => {
-    if (window.pageYOffset.toFixed() === scrollTo.toFixed()) {
-      window.removeEventListener("scroll", scrollCallback);
-      callback();
-    }
-  };
-  window.addEventListener("scroll", scrollCallback);
-  scrollCallback();
   window.scrollTo({
     top: scrollTo,
     behavior: "smooth",
   });
+  callback();
 }
